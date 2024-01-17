4c62221764fba6adb1a52ef495651636bdf2f297
osip123 <131646261+osip123@users.noreply.github.com>
2024-01-17 16:55:00 +0300

    test

--- v2/src/script.js
+++ v2/src/script.js
@@ -1,8 +1,11 @@
 import { Core } from "../core/core.js";
 import { Player } from "../core/player.js";
+import { Uliles } from "../utiles/Utiles.js";
 
 const core = new Core;
 const player = new Player;
+const uliles = new Uliles;
 
 core._Init();
 player._Loop();
+uliles.calcFps();
