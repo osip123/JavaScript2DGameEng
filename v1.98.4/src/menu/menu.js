bee062cbdbdeff37cadb95660e7ca79d85651c34
osip123 <131646261+osip123@users.noreply.github.com>
2024-05-09 16:58:13 +0300

    menu render

---
+++ v1.98.4/src/menu/menu.js
@@ -0,0 +1,11 @@
+// import { renderMenu } from "./render"
+
+let menu = document.getElementById('app')
+
+let va = `<h1>hek</h1>`
+
+menu.innerHTML(va)
+
+// renderMenu(menu)
+
+console.log(menu)
---
+++ v1.98.4/src/menu/render.js
@@ -0,0 +1,6 @@
+export function renderMenu(menu){
+    let menu_items = `
+    <h1>hek</h1>
+    `
+    menu.innerHTML(menu_items)
+}
